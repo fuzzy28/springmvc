77422f6ab4ba4d337773250eccb6aa034475334f
Joel F. Ruelos Jr. <ruelosjoel@yahoo.com>
2016-02-01 20:56:33 +0800

    Initial Commit

---
+++ src/main/java/org/jrue/poc/springmvc/controller/HomeController.java
@@ -0,0 +1,26 @@
+package org.jrue.poc.springmvc.controller;
+
+import java.util.Map;
+
+import org.jrue.poc.springmybatis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	private UserService userService;
+	
+	@Autowired
+	public HomeController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@RequestMapping(value={"/", "/home"}, method = RequestMethod.GET)
+	public String showHomePage(Map<String,Object> model) {
+		model.put("users", userService.findAll());
+		return "home";
+	}
+}
---
+++ src/test/java/org/jrue/poc/springmvc/controller/HomeControllerTest.java
@@ -0,0 +1,44 @@
+package org.jrue.poc.springmvc.controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jrue.poc.springmybatis.domain.User;
+import org.jrue.poc.springmybatis.service.UserService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+public class HomeControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		List<User> users = Arrays.asList(new User(), new User(), new User());
+		UserService userService = Mockito.mock(UserService.class);
+		
+		Mockito.when(userService.findAll())
+			   .thenReturn(users);
+		
+		HomeController controller = new HomeController(userService);
+		
+		Map<String,Object> model = new HashMap<String,Object>();
+		String view = controller.showHomePage(model);
+		
+		Assert.assertEquals("home", view);
+		
+		Assert.assertSame(users, model.get("users"));
+		Mockito.verify(userService).findAll();
+	}
+
+}
