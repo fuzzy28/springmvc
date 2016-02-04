0448022dd29d26f449c1b8c481900ab473de84e9
Joel F. Ruelos Jr. <ruelosjoel@yahoo.com>
2016-02-04 17:59:42 +0800

    Removed unnecessary methods

--- src/test/java/org/jrue/poc/springmvc/controller/HomeControllerTest.java
+++ src/test/java/org/jrue/poc/springmvc/controller/HomeControllerTest.java
@@ -7,20 +7,10 @@ import java.util.Map;
 
 import org.jrue.poc.springmybatis.domain.User;
 import org.jrue.poc.springmybatis.service.UserService;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 public class HomeControllerTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void test() {
